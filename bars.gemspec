89832b0b04842a15373acd8e135ececa3b63bdd2
Robert Fletcher <lobatifricha@gmail.com>
2011-05-14 22:25:32 -0700

    first commit

---
+++ bars.gemspec
@@ -0,0 +1,15 @@
+Gem::Specification.new do |s|
+  s.name = %q{bars}
+  s.version = "0.0.1"
+  s.date = %q{2011-05-14}
+  s.authors = ["Robert Fletcher"]
+  s.email = %q{lobatifricha@gmail.com}
+  s.summary = %q{Keep track of your progress towards goals with bars}
+  s.homepage = %q{http://www.knowledgepile.net}
+  s.description = %q{bars reads ~/pbars.yml and outputs the result in bar format}
+  s.files = %w[
+    lib/bars.rb
+    bin/bars
+  ]
+  s.executables = ["bars"]
+end
---
+++ lib/bars.rb
@@ -0,0 +1,31 @@
+require 'yaml'
+
+class Bars
+  def initialize(args=[])
+    if !args.empty? && args.first == '-p'
+      @print_mode = :percentage
+    else
+      @print_mode = :bars
+    end
+
+    @timeframes = YAML.load(File.open(File.expand_path('~/bars.yml')))
+  end
+
+  def print
+    puts
+    @timeframes.each do |timeframe, tasks|
+      puts " #{timeframe.capitalize} ".center(30, "*")
+      tasks.each do |task, done, goal|
+        if @print_mode == :percentage
+          puts "--> #{task}: #{(done.to_f/goal * 100).round}%"
+        else
+          puts "--> #{task}:"
+          done_count = (done.to_f/goal * 20).to_i
+          todo_count = 20 - done_count
+          puts "    |#{"+" * (done_count - 1)}|#{"-" * todo_count}|"
+        end
+      end
+      puts
+    end
+  end
+end
