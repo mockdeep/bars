3668e841190daa7680d663f349d9a8c451d7b037
Robert Fletcher <lobatifricha@gmail.com>
2011-05-15 18:46:20 -0700

    pulling options into print method

--- bars.gemspec
+++ bars.gemspec
@@ -1,12 +1,12 @@
 Gem::Specification.new do |s|
   s.name = %q{bars}
-  s.version = "0.0.5"
-  s.date = %q{2011-05-14}
+  s.version = "0.0.6"
+  s.date = %q{2011-05-15}
   s.authors = ["Robert Fletcher"]
   s.email = %q{lobatifricha@gmail.com}
   s.summary = %q{Keep track of your progress towards goals with bars}
-  s.homepage = %q{http://www.knowledgepile.net}
-  s.description = %q{bars reads ~/pbars.yml and outputs the result in bar format}
+  s.homepage = %q{https://github.com/mockdeep/bars}
+  s.description = %q{bars reads ~/bars.yml and outputs the result in bar format}
   s.files = %w[
     lib/bars.rb
     bin/bars
--- lib/bars.rb
+++ lib/bars.rb
@@ -2,23 +2,25 @@ require 'yaml'
 
 class Bars
   def initialize(options={})
-    @print_mode = options.delete(:print_mode) { :bars }
-    @bar_size = options.delete(:bar_size) { 20 }
-    @timeframes = YAML.load(File.open(File.expand_path('~/bars.yml')))
+    file_path = options.delete(:file_path) { '~/bars.yml' }
+    @timeframes = YAML.load(File.open(File.expand_path(file_path)))
   end
 
-  def print
+  def print(options={})
+    print_mode = options.delete(:print_mode) { 'bars' }
+    bar_size   = options.delete(:bar_size) { 20 } - 1
+
     puts
     @timeframes.each do |timeframe, tasks|
       puts " #{timeframe.capitalize} ".center(30, "*")
       tasks.each do |task, done, goal|
         done_percent = (done.to_f/goal)
-        if @print_mode == :percentage
+        if print_mode == 'percent'
           puts "--> #{task}: #{(done_percent * 100).floor}%"
         else
           puts "--> #{task}:"
-          done_string = "+" * (done_percent * @bar_size)
-          todo_string = "-" * (@bar_size - done_string.length)
+          done_string = "+" * (done_percent * bar_size)
+          todo_string = "-" * (bar_size - done_string.length)
           if done == 0
             splitter = '-'
           elsif done == goal
