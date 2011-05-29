0e2f5ab69639a5b74a95186d17968c4649500b45
Robert Fletcher <lobatifricha@gmail.com>
2011-05-28 23:24:28 -0700

    cleaning up the look

--- lib/bars.rb
+++ lib/bars.rb
@@ -7,31 +7,19 @@ class Bars
   end
 
   def print(options={})
-    print_mode = options.delete(:print_mode) { 'bars' }
-    line_width = options.delete(:line_width) { 20 } - 1
+    line_width = options.delete(:line_width) { 25 } - 1
 
     puts
     @timeframes.each do |timeframe, tasks|
-      puts " #{timeframe.capitalize} ".center(30, "*")
       tasks.each do |task, done, goal|
-        done_percent = (done.to_f/goal)
-        if print_mode == 'percent'
-          left_side = "#{task}:".ljust(line_width)
-          right_side = "#{(done_percent * 100).floor}%".rjust(4)
-          puts "--> #{left_side}#{right_side}"
-        else
-          puts "--> #{task}:"
-          done_string = "+" * (done_percent * line_width)
-          todo_string = "-" * (line_width - done_string.length)
-          if done == 0
-            splitter = '-'
-          elsif done == goal
-            splitter = '+'
-          else
-            splitter = '|'
-          end
-          puts "    |#{done_string}#{splitter}#{todo_string}|"
-        end
+        percent = (done.to_f/goal)
+        count = (percent * line_width.to_f).round
+        done_string = "█" * (count)
+        todo_string = "┅" * (line_width - count)
+        percent_string = sprintf("%.1f%", (percent * 100)).rjust(6)
+        goal_string = "#{done}/#{goal}".rjust(7)
+        puts "#{task.ljust(line_width - 7)}#{goal_string}"
+        puts "#{done_string}#{todo_string}#{percent_string}"
       end
       puts
     end
